0b1749305fb5d2c90c82da7ef4dccd53ee7a04d8
jcvela76 <jcvela@gmail.com>
2016-08-05 12:38:26 -0400

    controller updated
    Simplify controller

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,37 +3,40 @@
 
   angular
     .module('template')
-    .controller('MainController', MainController);
+    .controller('MainController', function MainController($timeout, webDevTec, toastr) 
+      {
+        var vm = this;
 
-  /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
+        vm.awesomeThings = [];
+        vm.classAnimation = '';
+        vm.creationDate = 1470412067245;
+        vm.showToastr = showToastr;
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1470412067245;
-    vm.showToastr = showToastr;
+        activate();
 
-    activate();
+        function activate() {
+          getWebDevTec();
+          $timeout(function() {
+            vm.classAnimation = 'rubberBand';
+          }, 4000);
+        }
 
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
+        function showToastr() {
+          toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+          vm.classAnimation = '';
+        }
 
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
+        function getWebDevTec() {
+          vm.awesomeThings = webDevTec.getTec();
 
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
+          angular.forEach(vm.awesomeThings, function(awesomeThing) {
+            awesomeThing.rank = Math.random();
+          });
+        }
+      }
 
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
-  }
+    );
+
+
+  
 })();
